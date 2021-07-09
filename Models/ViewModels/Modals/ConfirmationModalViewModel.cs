808ccad36d18b0e6b2cbb7d302e0974a0eff68da
tHeather <wrzosek46@gmail.com>
2021-07-09 21:19:11 +0200

    Add confirmation modal

---
+++ Models/ViewModels/Modals/ConfirmationModalViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineConsulting.Models.ViewModels.Modals
+{
+    public class ConfirmationModalViewModel
+    {
+        public string Id { get; set; }
+        public string ModalLabel { get; set; }
+        public string ModalText { get; set; }
+
+        public string ConfirmController { get; set; }
+
+        public string ConfirmAction { get; set; }
+
+        public Dictionary<string,string> ConfirmParams { get; set; }
+
+        public string ConfrimText { get; set; }
+
+        public string  RejectText { get; set; }
+    }
+}
