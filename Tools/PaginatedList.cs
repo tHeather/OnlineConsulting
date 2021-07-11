db6e41e0658219ee76abce9568eec8e9a3a7cabd
tHeather <wrzosek46@gmail.com>
2021-07-11 11:38:22 +0200

    separate pagination component

--- Tools/PaginatedList.cs
+++ Tools/PaginatedList.cs
@@ -13,9 +13,10 @@ namespace OnlineConsulting.Tools
 
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
-            PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
 
+            PageIndex = pageIndex > TotalPages? TotalPages: pageIndex < 1? 1 :pageIndex;
+       
             this.AddRange(items);
         }
 
@@ -35,8 +36,19 @@ namespace OnlineConsulting.Tools
             }
         }
 
+        public List<int> GetSurroundingIndexes(int maxSideRange)
+        {
+
+           var nextRangeEndIndex = TotalPages >= PageIndex + maxSideRange ? PageIndex + maxSideRange : TotalPages;
+           var prevRangeEndIndex = PageIndex - maxSideRange > 1 ? PageIndex - maxSideRange : 1;
+
+           return Enumerable.Range(prevRangeEndIndex, nextRangeEndIndex - prevRangeEndIndex + 1).ToList();
+        }
+
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
+            if (pageIndex < 1) pageIndex = 1;
+
             var count = await source.CountAsync();
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
