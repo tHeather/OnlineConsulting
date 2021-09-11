bfec207836f825a3ddb591f2c6c22aa75ff952ef
tHeather <wrzosek46@gmail.com>
2021-09-11 10:17:12 +0200

    Add user name and surname to statistics

--- Controllers/StatisticController.cs
+++ Controllers/StatisticController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OnlineConsulting.Constants;
+using OnlineConsulting.Services.Repositories.Interfaces;
 
 namespace OnlineConsulting.Controllers
 {
@@ -8,6 +9,13 @@ namespace OnlineConsulting.Controllers
     [Route("statistics")]
     public class StatisticController : Controller
     {
+        private readonly IUserRepository _userRepository;
+
+        public StatisticController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public IActionResult GetStatistics()
         {
             return View();
@@ -16,7 +24,8 @@ namespace OnlineConsulting.Controllers
         [HttpGet("{id}")]
         public IActionResult GetStatisticsForConsultant(string id)
         {
-            return View("GetStatistics", id);
+            var consultant = _userRepository.GetUserById(id);
+            return View("GetStatistics", consultant);
         }
 
     }
