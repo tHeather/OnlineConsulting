276a3ceb3834424a0eda2ba2671cfdacd74d9a4b
tHeather <wrzosek46@gmail.com>
2021-10-24 19:21:08 +0200

    Add authorization to change password page

--- Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using OnlineConsulting.Constants;
 using OnlineConsulting.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace OnlineConsulting.Areas.Identity.Pages.Account.Manage
 {
+    [Authorize(Roles = UserRoleValue.ADMIN + "," + UserRoleValue.CONSULTANT + "," + UserRoleValue.EMPLOYER)]
     public class ChangePasswordModel : PageModel
     {
         private readonly UserManager<User> _userManager;
